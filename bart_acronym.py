9a4a4430c970abea06f25c226eba12769df2e588
Boxiang Liu <jollier.liu@gmail.com>
2021-08-18 17:38:13 -0700

    new file:   bart_acronym.py 	new file:   biobert_acronym.py 	modified:   requirements.txt 	new file:   t5_acronym.py

---
+++ bart_acronym.py
@@ -0,0 +1,18 @@
+from transformers.pipelines import pipeline
+import sys
+
+model_name = "phiyodr/bart-large-finetuned-squad2"
+nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+
+def answer_question(question, doc):
+    inputs = {"question": question, 
+              "context": doc}
+    res = nlp(inputs)
+    return res["answer"]
+
+for line in sys.stdin:
+    fields = line.rstrip().split("\t")
+    if len(fields) >= 2:
+        SF, doc = fields[0:2]
+        LF = answer_question("What does {} stand for?".format(SF), doc)
+        print("\t".join([LF, SF, doc]))
---
+++ t5_acronym.py
@@ -0,0 +1,20 @@
+from transformers import T5Config, T5ForConditionalGeneration, T5Tokenizer
+import sys
+
+model_name = "allenai/t5-small-squad11"
+tokenizer = T5Tokenizer.from_pretrained(model_name)
+model = T5ForConditionalGeneration.from_pretrained(model_name)
+
+def answer_question(question, doc):
+    input_string = "{} \n {}".format(question, doc)
+    input_ids = tokenizer.encode(input_string, return_tensors="pt")
+    res = model.generate(input_ids)
+    output = tokenizer.batch_decode(res, skip_special_tokens=True)
+    return output
+
+for line in sys.stdin:
+    fields = line.rstrip().split("\t")
+    if len(fields) >= 2:
+        SF, doc = fields[0:2]
+        LF = answer_question("What does {} stand for?".format(SF), doc)[0]
+        print("\t".join([LF, SF, doc]))
